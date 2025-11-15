dbedc9b5b8f0a64c6848ac916ef1223b5aafa4ea
Ananya Srivastava <ananyaaa3080@gmail.com>
2025-11-15 20:53:42 +0530

    initializing repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import image from './assets/01.png'
+
+function App() {
+
+  return (
+  <div>
+  <h1>ananya</h1>
+  <p> himanshu is an idiot</p>
+  <img src={image} 
+  width={500} height={500}></img>
+  </div>
+  )
+}
+
+export default App
